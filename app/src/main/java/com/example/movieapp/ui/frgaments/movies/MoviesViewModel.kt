9d163365cb7929c807cfd52028321784e0e3a3ae
youssef hesham <64506232+youssefhesham2@users.noreply.github.com>
2022-10-08 20:00:24 +0200

    Add the MoviesViewModel.

---
+++ app/src/main/java/com/example/movieapp/ui/frgaments/movies/MoviesViewModel.kt
@@ -0,0 +1,78 @@
+package com.example.movieapp.ui.frgaments.movies
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.data.repository.MoviesRepositoryImpl
+import com.example.domain.entities.ErrorResponseDomainEntity
+import com.example.domain.entities.ResultsDomainEntity
+import com.example.domain.repository.MoviesRepository
+import com.example.domain.usecase.GetMostPopularMoviesUseCase
+import com.example.domain.usecase.GetTopRatedMoviesUseCase
+import com.example.domain.usecase.getMostPopularMoviesUseCaseImpl
+import com.example.domain.usecase.getTopRatedMoviesUseCaseImpl
+import com.example.domain.utils.DataState
+import com.example.movieapp.utils.Constants
+import com.example.movieapp.utils.LoadingState
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+
+class MoviesViewModel(
+    private val moviesViewModelJob: Job = SupervisorJob(),
+    private val mainDispatcher: CoroutineDispatcher = Dispatchers.Main,
+    private val IODispatcher: CoroutineDispatcher = Dispatchers.IO,
+    private val moviesRepository: MoviesRepository = MoviesRepositoryImpl(),
+    private val getMostPopularMoviesUseCase: GetMostPopularMoviesUseCase = getMostPopularMoviesUseCaseImpl,
+    private val getTopRatedMoviesUseCase: GetTopRatedMoviesUseCase = getTopRatedMoviesUseCaseImpl
+) : ViewModel(), CoroutineScope {
+    private val TAG = "MoviesViewModel"
+    val loading = MutableLiveData<LoadingState>()
+    val movies = MutableLiveData<List<ResultsDomainEntity>>()
+    val edgeCase = MutableLiveData<String>()
+    val failure = MutableLiveData<ErrorResponseDomainEntity>()
+    val exception = MutableLiveData<Throwable>()
+
+    fun fetchMostPopularMovies(isInternetConnected: Boolean) {
+        loading.postValue(LoadingState.SHOW)
+        launch {
+            val response =
+                withContext(IODispatcher) {
+                    getMostPopularMoviesUseCase.invoke(moviesRepository, isInternetConnected)
+                }
+            handleGetMoviesResponse(response)
+        }
+    }
+
+    fun fetchTopRatedMovies(isInternetConnected: Boolean) {
+        loading.postValue(LoadingState.SHOW)
+        launch {
+            val response =
+                withContext(IODispatcher) {
+                    getTopRatedMoviesUseCase.invoke(moviesRepository, isInternetConnected)
+                }
+            handleGetMoviesResponse(response)
+        }
+    }
+
+    private fun handleGetMoviesResponse(response: DataState) {
+        loading.postValue(LoadingState.DISMISS)
+        when (response) {
+            is DataState.Successful<*> -> movies.postValue(response.result as List<ResultsDomainEntity>)
+            is DataState.Empty -> edgeCase.postValue(response.message)
+            is DataState.Failure<*> -> failure.postValue(response.errorResponse as ErrorResponseDomainEntity)
+            else -> edgeCase.postValue(Constants.EMPTY)
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        moviesViewModelJob.cancel()
+    }
+
+    override val coroutineContext: CoroutineContext
+        get() = mainDispatcher + moviesViewModelJob + CoroutineName("🙄 moviesViewModel Scope") + CoroutineExceptionHandler { coroutineContext, throwable ->
+            Log.e(TAG, "🤬 Exception $throwable in context:$coroutineContext")
+            loading.postValue(LoadingState.DISMISS)
+            exception.postValue(throwable)
+        }
+}
