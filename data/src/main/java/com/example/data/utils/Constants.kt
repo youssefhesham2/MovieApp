06155375adf09de5182c8f57121f3439811675a4
youssef hesham <64506232+youssefhesham2@users.noreply.github.com>
2022-10-08 18:31:35 +0200

    Add the data layer constants.

---
+++ data/src/main/java/com/example/data/utils/Constants.kt
@@ -0,0 +1,14 @@
+package com.example.data.utils
+
+object Constants {
+    const val QUERY_PARAMS_API_KEY = "api_key"
+    const val POSTER_SIZE = "w185"
+
+    const val START_GET_POPULAR_MOVIE_FROM_REMOTE="Start fetch popular movie from remote..."
+    const val FETCHED_POPULAR_MOVIE_FROM_REMOTE_SUCCESSFULLY="Popular movies have been successfully fetched from the remote"
+    const val FETCHED_POPULAR_MOVIE_FROM_REMOTE_FAILURE="Failed to fetch popular movies from remote"
+
+    const val START_GET_TOP_RATED_MOVIES_FROM_REMOTE="Start fetch top rated movies from remote..."
+    const val FETCHED_TOP_RATED_MOVIES_FROM_REMOTE_SUCCESSFULLY="Top rated movies have been successfully fetched from the remote"
+    const val FETCHED_TOP_RATED_MOVIES_FROM_REMOTE_FAILURE="Failed to fetch top rated movies from remote"
+}
