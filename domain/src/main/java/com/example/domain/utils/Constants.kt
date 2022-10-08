66e86b8ae4a02a425c4cedd15cff56cd0894e58a
youssef hesham <64506232+youssefhesham2@users.noreply.github.com>
2022-10-08 18:21:15 +0200

    Add the domain Constants.

---
+++ domain/src/main/java/com/example/domain/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.domain.utils
+
+object Constants {
+    const val NETWORK_ERROR = "Network error!\n please connect to the internet."
+    const val EMPTY_DATA="NO Data Found!"
+}
