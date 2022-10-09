c8e6802db79c254197c0cebedef74a69b6cc9a1d
youssef hesham <64506232+youssefhesham2@users.noreply.github.com>
2022-10-10 00:55:11 +0200

    Add constants.

--- app/src/main/java/com/example/movieapp/utils/Constants.kt
+++ app/src/main/java/com/example/movieapp/utils/Constants.kt
@@ -3,4 +3,6 @@ package com.example.movieapp.utils
 object Constants {
     const val LOADING="Loading..."
     const val EMPTY="NO Data Found please try aging soon!"
+
+    const val ITEM_SETTING_MENU="idLastItemSelectedFromSettingsMenu"
 }
